1375e58f7732444dc1fcefef12eb36fc404a5edc
Claire <clairebrunelx@gmail.com>
2021-10-28 17:06:32 +0200

    affichage props name & id

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import TestNeo from './components/TestNeo';
-import NeoInfo from './components/Neo';
+import NeoInfo from './Neo';
 
 export default function App() {
   return (
     <div>
       <h1>marre de cet api</h1>
-
       <div>
         {NeoInfo.map((objets) => (
-          <TestNeo key={objets.name} />
+          <TestNeo
+            name={objets.name}
+            id={objets.id}
+            magnitude={objets.absolute_magnitude_h}
+            neo={objets.neo_reference_id}
+          />
         ))}
       </div>
     </div>
--- src/components/TestNeo.jsx
+++ src/components/TestNeo.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 
-const TestNeo = ({ name }) => (
+const TestNeo = ({
+  name,
+  id,
+  absolute_magnitude_h: magnitude,
+  neo_reference_id: neoid,
+}) => (
   <div>
     <h3>name: {name}</h3>
+    <h3>id: {id}</h3>
+    <h3>magnitude: {magnitude}</h3>
+    <h3>neooo: {neoid}</h3>
   </div>
 );
 
