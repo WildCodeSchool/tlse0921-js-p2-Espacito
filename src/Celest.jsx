a233da14a0d5255fc3c3b23147b6d6eb29e87693
Claire <clairebrunelx@gmail.com>
2021-11-05 10:15:04 +0100

    ajout (test map)sampleNeoCard.jsx & promise

--- src/Celest.jsx
+++ src/Celest.jsx
@@ -1,28 +1,32 @@
 import axios from 'axios';
 import { useState } from 'react';
+import NeoCard from './components/NeoCard';
 
 function Asteroids() {
-  // eslint-disable-next-line no-unused-vars
   const [asteroids, setAsteroids] = useState([]);
-  function getAsteroids() {
-    axios
-      .get(
-        'https://api.nasa.gov/neo/rest/v1/feed?api_key=us2MndB2OwcMCT887aznbmPtSzxIs5FydFkRGnNy&start_date=2021-11-01&end_date=2021-11-07',
-      )
-      .then((res) => res.data.near_earth_objects)
-      // .then(() => {
-
-      // })
-      .then((data) => {
-        setAsteroids(data[0]);
-      });
-  }
+  axios
+    .get(
+      'https://api.nasa.gov/neo/rest/v1/feed?api_key=us2MndB2OwcMCT887aznbmPtSzxIs5FydFkRGnNy&start_date=2021-11-01&end_date=2021-11-07',
+    )
+    .then((res) => res.data.near_earth_objects)
+    .then((data) => {
+      setAsteroids(data);
+    });
 
   return (
     <div>
-      <button type="button" onClick={getAsteroids}>
-        get asteroids
-      </button>
+      {asteroids.map((objets) => (
+        <NeoCard
+          name={objets.name}
+          id={objets.id}
+          close_approach_date_full={
+            objets.close_approach_data[0].close_approach_date_full
+          }
+          is_potentially_hazardous_asteroid={
+            objets.is_potentially_hazardous_asteroid ? 'true' : 'false'
+          }
+        />
+      ))}
     </div>
   );
 }
--- src/components/NeoCard.jsx
+++ src/components/NeoCard.jsx
@@ -1,12 +1,10 @@
 const NeoCard = ({
-  element_count: element,
   name,
   id,
   is_potentially_hazardous_asteroid: dangerous,
   close_approach_date_full: date,
 }) => (
   <div>
-    <p>count: {element}</p>
     <p>name: {name}</p>
     <p>neo id: {id}</p>
     <p>
