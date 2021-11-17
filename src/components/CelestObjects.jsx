74dd4337e2ee8e226439f35ee8ddaf2cfa21d2cc
Tbiarneix <thomas.biarneix@gmail.com>
2021-11-17 15:59:40 +0100

    debug celestobjects

---
+++ src/components/CelestObjects.jsx
@@ -0,0 +1,9 @@
+import Asteroids from './Neo';
+import NeoCard from './NeoCard';
+
+const CelestObjects = () => (
+  <div>
+    <Asteroids NeoCard={NeoCard} />
+  </div>
+);
+export default CelestObjects;
