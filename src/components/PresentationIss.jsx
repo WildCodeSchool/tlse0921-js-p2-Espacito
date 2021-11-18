f1a23a3007faa50b06d1f1b5926154ea392440c2
Tbiarneix <thomas.biarneix@gmail.com>
2021-11-18 12:19:11 +0100

    suppression de la ligne blanche entre le menu et le header vidéo + augmentation de l'espace entre les tips et l'équipage

--- src/components/PresentationIss.jsx
+++ src/components/PresentationIss.jsx
@@ -10,10 +10,11 @@ import Astronauts from './Astronauts';
 const Hero = styled.div`
   display: flex;
   position: relative;
+  top: -1px;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  padding: 1rem;
+  padding: 2rem;
 
   h1 {
     color: white;
@@ -50,7 +51,7 @@ const HeroVideo = styled.video`
 const IssContainer = styled.div`
   width: 65%;
   list-style: none;
-  margin: 0 auto;
+  margin: 3rem auto 4rem;
 
   li {
     text-align: center;
